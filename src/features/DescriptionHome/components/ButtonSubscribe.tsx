a42611f1f669b5e7bcf9f05f597eb3bf274ba2fc
reissouza <reis.souza@outlook.com.br>
2022-07-03 09:06:09 -0300

    feat: :sparkles: redirection fro stripe

--- src/features/DescriptionHome/components/ButtonSubscribe.tsx
+++ src/features/DescriptionHome/components/ButtonSubscribe.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { signIn, useSession } from 'next-auth/react';
-import { ButtonBase } from '@/components';
+
+import { api } from '@/lib/axios';
+import { getStripeJs } from '@/services';
 
 import S from './styles.module.css';
+import { ButtonBase } from '@/components';
 
 type ButtonSubscribeProps = {
   priceId: string;
 };
 const ButtonSubscribe: React.FC<ButtonSubscribeProps> = ({ priceId }: ButtonSubscribeProps) => {
   const { data: session } = useSession();
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
     if (!session) {
       signIn('github');
+    }
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
     }
   };
   return (
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,6 +1,7 @@
 import { Client } from 'faunadb';
 
-export const fauna = new Client({
+const fauna = new Client({
   secret: process.env.FAUNADB_KEY,
   domain: 'db.us.fauna.com',
 });
+export default fauna;
