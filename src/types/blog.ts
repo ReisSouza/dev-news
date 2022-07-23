e79f8809139ef2f55cf43247a1111cc5a2195a5f
reissouza <reis.souza@outlook.com.br>
2022-07-23 16:58:49 -0300

    refactor: :label: adjustes export type and name type post for blog

---
+++ src/types/blog.ts
@@ -0,0 +1,8 @@
+type Blog = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updated: string;
+};
+
+export default Blog;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
 export type { default as RouterTypes } from './routes';
 export type { default as ProductType } from './product';
+export type { default as BlogType } from './blog';
+export type { default as PostType } from './post';
